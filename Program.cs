1db6f52cf8d8c85a46417067b97eb0e210e6c8e6
juescoryisus <125850604+juescoryisus@users.noreply.github.com>
2023-08-31 22:45:04 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recursividad
+{
+   
+       class Program
+       {
+            static int CalcularFactorial(int n)
+            {
+                if (n == 0 || n == 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return n * CalcularFactorial(n - 1);
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                Console.Write("Ingrese un número: ");
+                int num = int.Parse(Console.ReadLine());
+                int resultado = CalcularFactorial(num);
+                Console.WriteLine($"El factorial de {num} es {resultado}");
+            }
+       }
+
+}
+
+
